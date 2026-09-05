499e64bb1260e46bca2f8fca58622426c6762c2c
VMahamadhBasha <vmahamadhbasha@gmail.com>
2026-09-05 20:41:36 +0530

    Time: 115 ms (93.18%), Space: 33.2 MB (32.73%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def firstStableIndex(self, nums: list[int], k: int) -> int:
+        m=0
+        sm=[0]*len(nums)
+        r=len(nums)-1
+        for i in range(len(nums)-1,-1,-1):
+            if nums[i]<=nums[r]:
+                sm[i]=nums[i]
+                r=i
+            else:
+                sm[i]=nums[r]
+            
+        for i in range(len(nums)):
+            if nums[i]>=nums[m]:
+                m=i
+            if nums[m]-sm[i]<=k:
+                return i
+        return -1
+
+        
+        
